d0c0ea4e25dcf4c2a839234b08650ccad3c2c790
suic86 <marko.gabriel.cz@gmail.com>
2025-04-16 19:08:56 +0200

    select example.

---
+++ select/racer.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	if _, err := http.Get(url); err != nil {
+		panic(err)
+	}
+	return time.Since(start)
+}
+
+var tenSecondTimeout = 10 * time.Second
+
+func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winning string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		if _, err := http.Get(url); err != nil {
+			panic(err)
+		}
+		close(ch)
+	}()
+	return ch
+}
