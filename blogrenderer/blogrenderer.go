16fa5e703264eee4445fee2ee62016cf07fb0315
Gabriel Marko <marko.gabriel.cz@gmail.com>
2025-04-23 22:47:46 +0200

    blogrenderer package (templating example)

---
+++ blogrenderer/blogrenderer.go
@@ -0,0 +1,42 @@
+package blogrenderer
+
+import (
+	"embed"
+	"html/template"
+	"io"
+	"strings"
+)
+
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
+type Post struct {
+	Title, Description, Body string
+	Tags                     []string
+}
+
+func (p Post) SanitisedTitle() string {
+	return strings.ToLower(strings.Replace(p.Title, " ", "-", -1))
+}
+
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	return r.templ.ExecuteTemplate(w, "blog.gohtml", p)
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	return r.templ.ExecuteTemplate(w, "index.gohtml", posts)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/suic86/learn-go-with-tests
 
 go 1.24.2
+
+require github.com/approvals/go-approval-tests v1.4.0
